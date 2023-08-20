25994da9b95e2148cfc4d49c1469d447bd7ad651
Florian Thiel <flth@flth.de>
2023-08-20 17:05:36 +0200

    initial

---
+++ strings.lua
@@ -0,0 +1,26 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "Carbon dioxide trolley",
+		["mod_desc"] = "Carl Gustav Rommenhöller was a German entrepreneur and pioneer of the carbonic acid industry that was spreading at the end of the 19th century. He founded the company Kohlensäurewerke C. G. Rommenhöller, colloquially known as \"Rommenhöllerwerke\", which owned a total of 40 carbonic acid plants, numerous carbonic acid springs and deposits at various locations in Europe (and in Africa until 1926).\nThe wagons were always painted white with an orange and yellow \"Bauchbinde\" and were thus easily visible in the train formation.",
+
+        ["Kohlensaeurewagen_menu"] = "Carbon dioxide trolley",
+		["Kohlensaeurewagen_1_name"] = "Carbon dioxide trolley Rommenhöller",
+		["Kohlensaeurewagen_2_name"] = "Carbon dioxide trolley AGA",
+        ["Kohlensaeurewagen_desc"] = "The C. G. Rommenhöller carbonic acid plant shipped its liquid carbonic acid to various locations throughout Europe. The carbonic acid was used, for example, for mineral water, beer pressure, cooling systems, petrol tanks or fire extinguishers.",
+
+	},
+	de = {
+
+		["mod_name"] = "Kohlensäurewagen",
+		["mod_desc"] = "Carl Gustav Rommenhöller war ein deutscher Unternehmer und Wegbereiter der sich Ende des 19. Jahrhunderts ausbreitenden Kohlensäureindustrie. Er gründete das umgangssprachlich kurz „Rommenhöllerwerke“ genannte Unternehmen Kohlensäurewerke C. G. Rommenhöller, dem an verschiedenen Standorten in Europa (und bis 1926 in Afrika) insgesamt 40 Kohlensäurewerke, zahlreiche Kohlensäuresprudel und -lagerstätten gehörten.\nDie Wagen waren stets weiß angestrichen mit einer orange-gelbe \"Bauchbinde\" und so gut sichtbar im Zugverband.",
+
+        ["Kohlensaeurewagen_menu"] = "Kohlensäurewagen",
+		["Kohlensaeurewagen_1_name"] = "Kohlensaeurewagen Rommenhöller",
+		["Kohlensaeurewagen_2_name"] = "Kohlensaeurewagen AGA",
+        ["Kohlensaeurewagen_desc"] = "Das Kohlensäurewerk C. G. Rommenhöller verschickte europaweit seine flüssige Kohlensäure an diverse Standort. Die Kohlensäure wurde genutzt z.B. für Mineralwasser, Bierdruck, Kühlanlagen, Benzintanks oder Feuerlöschapparate.",
+
+	},
+}
+end
